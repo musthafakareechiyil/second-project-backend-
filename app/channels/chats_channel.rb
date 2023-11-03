883f3d1f1aaedb8f5471a6b6eaf7293a84b70f2b
musthafa <musthafakareechiyil@gmail.com>
2023-11-03 13:48:48 +0530

    chat working fine

--- app/channels/chats_channel.rb
+++ app/channels/chats_channel.rb
@@ -1,11 +1,10 @@
 class ChatsChannel < ApplicationCable::Channel
   def subscribed
-    stop_all_streams
     conversation_id = [params[:sender_id], params[:receiver_id]].sort.join('_')
     stream_from "chat_#{conversation_id}"
   end
 
   def unsubscribed
-    stop_all_streams
+    # stop_all_streams
   end
 end
