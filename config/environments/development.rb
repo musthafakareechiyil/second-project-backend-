3232411e11c6db752a882f8144b96347856aa6e4
musthafa <musthafakareechiyil@gmail.com>
2023-11-24 21:15:10 +0530

    action cable development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_cable.allowed_request_origins = [%r{http://*}, %r{https://*}]
+  Rails.application.config.action_cable.disable_request_forgery_protection = true
 end
